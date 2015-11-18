090b59d4b12bf37aee76677d8c035e7186d2ae98
danielmonr <danielmonr@gmail.com>
2015-11-18 12:43:14 -0600

    Agregar Activity de RSA y SHA

---
+++ app/src/main/java/com/danielmonr/algoritmosseguridad/SHA.java
@@ -0,0 +1,14 @@
+package com.danielmonr.algoritmosseguridad;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class SHA extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sha);
+    }
+
+}
